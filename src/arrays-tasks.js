5c471930d625c7cfbbef8163dfa756bad1cffea8
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-11-15 21:38:22 +0100

    feat: implement findElement

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -61,8 +61,10 @@ function sumArrays(arr1, arr2) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  const result = arr.indexOf(value);
+
+  return result;
 }
 
 /**
