9511d7c83a790e490066252566c77aa4d2df5cca
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-11-15 21:39:46 +0100

    feat: implement findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -81,8 +81,8 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((sum, value) => (value === item ? sum + 1 : sum), 0);
 }
 
 /**
