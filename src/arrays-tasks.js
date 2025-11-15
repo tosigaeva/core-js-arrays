0563af567332034a321e262ca796fd72ef02e3cf
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-11-15 22:49:56 +0100

    feat: implement calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -331,8 +331,11 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce(
+    (balance, [income, expence]) => balance + (income - expence),
+    0
+  );
 }
 
 /**
