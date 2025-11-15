378b628aa7b261c860099241e55b0f028b7075b7
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-11-15 22:07:50 +0100

    feat: implement isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -147,8 +147,8 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((item) => item.length === arr[0].length);
 }
 
 /**
