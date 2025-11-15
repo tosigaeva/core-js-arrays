037b99866c2e4007a7842cd56e40ea5c380d91b7
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-11-15 23:18:00 +0100

    feat: implement generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -370,8 +370,8 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return new Array(len).fill().map((_, index) => 2 * index + 1);
 }
 
 /**
